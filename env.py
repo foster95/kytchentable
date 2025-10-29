18f8f0e65138b1ee8039a7b12eabfa610476f108
foster95 <alicerosefoster95@gmail.com>
2025-10-29 16:40:42 +0000

    set up prostgres instance

---
+++ env.py
@@ -0,0 +1,3 @@
+import os 
+
+os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_2Z5lPnjsJEVT@ep-spring-waterfall-ag17ybz1.c-2.eu-central-1.aws.neon.tech/say_vowel_grass_82502") 
--- kytchen_table/wsgi.py
+++ kytchen_table/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kychen_table project.
+WSGI config for kytchen_table project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
