d30167b3ede0914e3767c077563d82985791c829
foster95 <alicerosefoster95@gmail.com>
2025-11-21 11:50:51 +0000

    add additional manual testing

--- menu/admin.py
+++ menu/admin.py
@@ -1,28 +1,15 @@
 from django.contrib import admin
-from django import forms
 from .models import MenuItem
 from .models import Allergen
 
 # Register your models here.
 
-
-class MenuItemAdminForm(forms.ModelForm):
-    class Meta:
-        model = MenuItem
-        fields = '__all__'
-        widgets = {
-            'allergens': forms.CheckboxSelectMultiple(),
-        }
-
-
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    form = MenuItemAdminForm
     list_display = ('name', 'category', 'tasting_order')
     list_editable = ('tasting_order',)
     list_filter = ('category',)
     ordering = ('category', 'tasting_order', 'name')
-
 
 @admin.register(Allergen)
 class AllergenAdmin(admin.ModelAdmin):
